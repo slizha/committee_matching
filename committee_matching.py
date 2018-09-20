acf2bd7e77208403931f505acc260cd5d55749c6
Parth Nobel <parthnobel@berkeley.edu>
2018-09-20 14:16:29 -0700

    Finishes the draft implementation

--- committee_matching.py
+++ committee_matching.py
@@ -3,22 +3,23 @@
 from __future__ import annotations
 
 import pandas as pd
+import sys
 from dataclasses import dataclass, field
-from typing import Tuple, List, Dict, Union
+from typing import Tuple, List, Dict, Optional
 
 
 @dataclass
 class Member:
     name: str
     preferred_committee: List[Committee] = field(default_factory=list)
-    on_a_string: Committee = None
+    on_a_string: Optional[Committee] = None
     rejected: List[Committee] = field(default_factory=list)
 
-    def recieve_offer(self, comm):
+    def recieve_offer(self, comm: Committee) -> bool:
         if self.on_a_string is None:
             on_a_string = comm
             return True
-        elif comm == self.preferred_committee.decide_between(
+        elif comm == self._decide_between(
                 on_a_string, comm):
             on_a_string.be_cut_loose(self)
             self.rejected.append(on_a_string)
@@ -35,6 +36,8 @@ class Member:
             elif opt == opt_2:
                 return opt_2
 
+        assert False
+
 
 @dataclass
 class Committee:
@@ -48,11 +51,15 @@ class Committee:
         return self.open_spots == len(self.waiting_on)
 
     def propose_to_next_member(self):
+        if self.satisfied():
+            return
         for _, member in sorted(
                 self.preferred_members.items(), key=lambda x: x[0]):
             if self not in member.rejected:
                 if member.recieve_offer(self):
                     self.waiting_on.append(member)
+
+                return
 
     def be_cut_loose(self, member: Member):
         self.waiting_on.remove(member)
@@ -60,7 +67,39 @@ class Committee:
 
 def load_csv(file_name: str) -> \
         Tuple[List[Committee], List[Member]]:
+    comms = generate_committees()
     df = pd.read_csv(file_name)
+    members = []
+    for _, row in df.iterrows():
+        member = Member(row.name)
+        member.preferred_committee.append(comms[row.first_choice])
+        member.preferred_committee.append(comms[row.second_choice])
+        member.preferred_committee.append(comms[row.third_choice])
+        member.preferred_committee.append(comms[row.fourth_choice])
+        member.preferred_committee.append(comms[row.fifth_choice])
+        member.preferred_committee.append(comms[row.sixth_choice])
+        members.append(member)
+
+        for comm in comms.values():
+            if comm.name == 'N/A':
+                continue
+            comm_preference = row[comm.name]
+            if comm_preference is None:
+                comm_preference = float('inf')
+            elif comm_preference in comm.preferred_members.keys():
+                print(comm.name, "repeated the number", comm_preference,
+                      file=sys.stderr)
+                comm_preference += 0.1 * [
+                    int(i)
+                    for i in comm.preferred_members.keys()
+                ].count(comm_preference)
+
+            if comm_preference in comm.preferred_members.keys():
+                raise RuntimeError("Something is very wrong.")
+
+            comm.preferred_members[comm_preference] = member
+
+    return list(comms.values()), members
 
 
 def generate_committees() -> Dict[str, Committee]:
@@ -71,5 +110,21 @@ def generate_committees() -> Dict[str, Committee]:
         Committee('Industrial Relations', 9),
         Committee('Public Relations', 9),
         Committee('Curriculum', 9),
+        Committee('N/A', 0)
     ]
     return {c.name: c for c in committees}
+
+
+def main():
+    committees, members = load_csv(sys.argv[1])
+
+    while any(not c.satisfied() for c in committees):
+        for committee in committees:
+            committee.propose_to_next_member()
+
+    for member in members:
+        print(member.name, "is on", member.on_a_string.name)
+
+
+if __name__ == '__main__':
+    main()
